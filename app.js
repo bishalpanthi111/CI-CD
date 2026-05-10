51809c36001b42ca2d52a9e655d5c578c43ad5a0
bishalpanthi111 <166199389+bishalpanthi111@users.noreply.github.com>
2026-05-10 20:59:00 +0300

    fix app.js to use callback style listen

--- app.js
+++ app.js
@@ -6,9 +6,6 @@ const PORT = process.env.PORT || 5001
 
 app.use(express.static('dist'))
 
-const start = async () => {
-  await app.listen(PORT)
+app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`)
-}
-
-start()
+})
